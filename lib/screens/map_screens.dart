3de53ec93986e960fddee0ed68eb5f61397a2fd4
Zotororonoa <mostacho_soto@hotmail.com>
2023-12-10 21:02:23 -0300

    bottomsheet agregado al marcador

--- lib/screens/map_screens.dart
+++ lib/screens/map_screens.dart
@@ -7,7 +7,7 @@ const MAPBOX_ACCESS_TOKEN =
     'pk.eyJ1Ijoiem90bzE5OTgiLCJhIjoiY2xvb3RxeXFrMDM2bzJrbzdhMXk4bWE1NiJ9.HvveZUcjGyb9ZCBJDnCziw';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
   State<MapScreen> createState() => _MapScreenState();
@@ -40,6 +40,37 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
+//Este es el Widget, por lo que entendi adentro debe llevar 
+//cualquier cosa que se pueda scrollear (DraggableScrollableSheet)
+
+//El resto ocurre en los marcadores abajito 
+
+void _showBottomSheet(BuildContext context, String markerName) {
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true, // Esto permite que el BottomSheet se mueva pa arriba
+    builder: (BuildContext context) {
+      return DraggableScrollableSheet(
+        expand: false,
+        builder: (BuildContext context, ScrollController scrollController) {
+          return Container(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                controller: scrollController,
+                child: Text('Bottom Sheet Content for $markerName'),
+              ),
+            ),
+          );
+        },
+      );
+    },
+  );
+}
+
+
+
+
   @override
   void initState() {
     getCurrentLocation();
@@ -49,100 +80,74 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Naka-map'),
-          backgroundColor: Colors.blueAccent,
-          actions: [
-            GestureDetector(
-              child: Icon(
-                estado
-              ),
-              onTap: (){
-                setState(()=> id='mapbox/navigation-night-v1');
-                setState(()=> estado= Icons.sunny);
-              },
-              onDoubleTap: (){
-                setState(()=> id='mapbox/streets-v12');
-                setState(()=> estado= Icons.nightlight);
-              },
-            ),
-            const SizedBox(
-              width: 40,
-            )
-          ],
-        ),
-        body:  myPosition == null
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Naka-map'),
+        backgroundColor: Colors.blueAccent,
+        actions: [
+          GestureDetector(
+            child: Icon(estado),
+            onTap: () {
+              setState(() => id = 'mapbox/navigation-night-v1');
+              setState(() => estado = Icons.sunny);
+            },
+            onDoubleTap: () {
+              setState(() => id = 'mapbox/streets-v12');
+              setState(() => estado = Icons.nightlight);
+            },
+          ),
+          const SizedBox(
+            width: 40,
+          )
+        ],
+      ),
+      body: myPosition == null
           ? const CircularProgressIndicator()
-          :FlutterMap(
-          options: MapOptions(
-              initialCenter: myPosition!,
-              minZoom: 5,
-              maxZoom: 25,
-              initialZoom: 10),
-          children: [
-            TileLayer(
-              urlTemplate:
-                  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-              additionalOptions: {
-                'accessToken': MAPBOX_ACCESS_TOKEN,
-                'id': id
-              },
-            ),
-            MarkerLayer(
-              markers: [
-                Marker(
-                  point: myPosition!,
-                  width: 90,
-                  height: 90,
-                  child: Image.asset('lib/assets/goingmerry.png'),
+          : FlutterMap(
+              options: MapOptions(
+                  initialCenter: myPosition!,
+                  minZoom: 5,
+                  maxZoom: 25,
+                  initialZoom: 10),
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+                  additionalOptions: {
+                    'accessToken': MAPBOX_ACCESS_TOKEN,
+                    'id': id,
+                  },
                 ),
-                Marker(
-                  point: const LatLng(-35.432460926832505, -71.63051261296629),
-                  width: 90,
-                  height: 90,
-                  child: Image.asset('lib/assets/polloKFC.png'), 
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: myPosition!,
+                      width: 90,
+                      height: 90,
+                      child: GestureDetector(
+                        onTap: () {
+                          _showBottomSheet(context, 'My Position');
+                        },
+                        child: Image.asset('lib/assets/goingmerry.png'),
+                      ),
+                    ),
+                    Marker(
+                      point: const LatLng(-35.432460926832505, -71.63051261296629),
+                      width: 90,
+                      height: 90,
+                      child: GestureDetector(
+                        onTap: () {
+                          _showBottomSheet(context, 'Marker 2');
+                        },
+                        child: Image.asset('lib/assets/polloKFC.png'),
+                      ),
+                    ),
+                    
+                  ],
                 ),
-                 Marker(
-                  point: const LatLng(-35.42620576785557, -71.65544307382392),
-                  width: 70,
-                  height: 70,
-                  child: Image.asset('lib/assets/DonasSIR.png'), 
-                ),
-                Marker(
-                  point: const LatLng(-35.428207782099655, -71.66229879996882),
-                  width: 70,
-                  height: 70,
-                  child: Image.asset('lib/assets/SakeMB.png'), 
-                ),
-                 Marker(
-                  point: const LatLng(-35.42330319594781, -71.66184818900518),
-                  width: 70,
-                  height: 70,
-                  child: Image.asset('lib/assets/SakeAGB.png'), 
-                ),
-                 Marker(
-                  point: const LatLng(-35.434504869577985, -71.61708282474858),
-                  width: 70,
-                  height: 70,
-                  child: Image.asset('lib/assets/SakeMPB.png'), 
-                ),
-                Marker(
-                  point: const LatLng(-35.43475236653571, -71.63060786343407),
-                  width: 70,
-                  height: 70,
-                  child: Image.asset('lib/assets/pizzaDP.png'), 
-                ),
-                Marker(
-                  point: const LatLng(-35.43491408600035, -71.62903877120327),
-                  width: 70,
-                  height: 70,
-                  child: Image.asset('lib/assets/pizzaMP.png'), 
-                ),
-        
               ],
             ),
-          ],
-        ));
+    );
   }
 }
+
